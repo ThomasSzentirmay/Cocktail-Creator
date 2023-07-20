cffd7b8066165f7e9aed72ad3aca0e40f672e51d
Thomas Szentirmay <tlszent@outlook.com>
2023-07-19 22:14:07 -0400

    debugging

--- config/connection.js
+++ config/connection.js
@@ -1,39 +1,16 @@
-// require('dotenv').config();
-// const Sequelize = require('sequelize');
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
-// module.exports = sequelize;
+const { Sequelize } = require('sequelize');
+const isProduction = process.env.PORT;
+let connection;
 
-require('dotenv').config();
-const Sequelize = require('sequelize');
+if (isProduction) {
+  connection = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql'
+  })
+} else {
+  connection = new Sequelize('tech_blog_db', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+}
 
-const dbConfig = {
-  host: 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  username: 'x1ig6tv311fm29t7',
-  password: 'otg0czj2n9jhisjv',
-  port: 3306,
-  database: 'heig1uuto8qdo5vt',
-};
-
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    port: dbConfig.port,
-    dialect: 'mysql',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  }
-);
-
-module.exports = sequelize;
+module.exports = connection;
